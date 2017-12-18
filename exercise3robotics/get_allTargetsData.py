21ad292fc1b199e4f936a7db0836c311a8211306
Daniel B <studentdanbu@gmail.com>
2017-12-18 20:58:07 +0100

    fixed shapes2222222

--- exercise3robotics/get_allTargetsData.py
+++ exercise3robotics/get_allTargetsData.py
@@ -72,7 +72,7 @@ for i in hp_sets:
 		if((rgb2gray(state.pob).shape[0] != 25) or (rgb2gray(state.pob).shape[1] != 25)):
 			print("Shape: ",state.pob.shape)
 			continue
-		print("Shape: ",rgb2gray(state.pob).shape)
+		print("Shape: ",rgb2gray(state.pob).shape,opt.state_siz)
 	    states[step, :] = rgb2gray(state.pob).reshape(opt.state_siz)
 	    labels[step]    = state.action
 
